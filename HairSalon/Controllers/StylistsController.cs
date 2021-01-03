4cb5708978e0b14e5fe1a698e699cd9273d53b7d
Tawnee Harris <tawneeh@icloud.com>
2021-01-03 13:13:43 -0800

    update Controllers

---
+++ HairSalon/Controllers/StylistsController.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+using HairSalon.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace HairSalon.Controllers
+{
+  public class StylistsController : Controller
+  {
+    private readonly HairSalonContext _db;
+    private StylistsController(HairSalonContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Stylist> model = _db.Stylists.ToList();
+      return View(model);
+    }
+
+  }
+}
