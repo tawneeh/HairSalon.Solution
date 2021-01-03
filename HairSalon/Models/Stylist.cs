3498565f6306e0bd49469d285452a8b3969a9061
Tawnee Harris <tawneeh@icloud.com>
2021-01-02 16:21:03 -0800

    add Models

---
+++ HairSalon/Models/Stylist.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+
+namespace HairSalon.Models
+{
+
+  public class Stylist
+  {
+
+    public Stylist()
+    {
+      this.Clients = new Hashset<Client>();
+    }
+
+    public int StylistId { get; set; }
+    public string Name { get; set; }
+    public string Specialty { get; set; }
+    public virtual ICollection<Client> Clients { get; set; }
+
+  }
+
+}
